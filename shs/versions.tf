0fabae80dba73a301e011c67d4be692a4fec6774
fmellek <ellek528@gmail.com>
2022-09-06 18:26:26 -0500

    syntax

--- shs/versions.tf
+++ shs/versions.tf
@@ -2,8 +2,8 @@ terraform {
   required_version = "~> 1.1.0"
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.7.0"
+        source = "hashicorp/aws"
+        version = "~> 4.7.0"
     }
   }
 }
