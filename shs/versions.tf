af22be5f89116c75654b76eb5d7dbaae3bd8b065
fmellek <ellek528@gmail.com>
2022-09-07 10:44:58 -0500

    version

--- shs/versions.tf
+++ shs/versions.tf
@@ -1,6 +1,9 @@
 terraform {
   required_version = "~> 0.12.0"
   required_providers {
-      aws = "~> 4.7.0"
+      aws = {
+      source = "hashicorp/aws"
+      version = "= 3.74.2"
+    }
   }
 }
