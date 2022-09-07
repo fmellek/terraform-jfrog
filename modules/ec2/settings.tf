175bd0b8e25147b95cff617d5360184333903181
fmellek <ellek528@gmail.com>
2022-09-07 10:36:01 -0500

    version has been updated.

--- modules/ec2/settings.tf
+++ modules/ec2/settings.tf
@@ -1,9 +1,7 @@
 terraform {
   required_version = "~> 1.1.0"
   required_providers {
-      aws = {
-          source = "hashicorp/aws"
-          version = "~> 4.7.0"
-      }
+      aws = "~> 4.7.0"
+      
   }
 }
