11c4da5ad90b59bcc699b9bda3070ba33f385c2c
fmellek <ellek528@gmail.com>
2022-09-07 10:23:19 -0500

    local call

--- shs/main.tf
+++ shs/main.tf
@@ -1,5 +1,5 @@
 module "ec2" {
-    source = "http://jfrog.tools.akumoproject.com:8082/artifactory/terraform-jfrog/"      #Local or Remote, when it is a Local Module, you provide a path of the Child Module
+    source = "../modules/ec2"      #Local or Remote, when it is a Local Module, you provide a path of the Child Module
     # You can declare your variables as a configuration
     ami = "ami-05fa00d4c63e32376"
     instance_type = "t2.micro"
